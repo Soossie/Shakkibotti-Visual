6caffd05c6171f446d67e9c78ac37bfeaf93faed
Soossie <87805341+Soossie@users.noreply.github.com>
2026-03-05 13:53:57 +0200

    Update ShakkiLogiikka.cs

--- Assets/Scripts/ShakkiLogiikka.cs
+++ Assets/Scripts/ShakkiLogiikka.cs
@@ -30,6 +30,8 @@ public class ShakkiLogiikka : MonoBehaviour
     
     void Start()
     {
+        GenerateGrid();
+        GeneratePieces();
         Aloitus();
         //kysy pelaajan väri
         _koneenVari = 1 - _pelaajanVari; // kone saa vastakkaisen värin
@@ -76,6 +78,53 @@ public class ShakkiLogiikka : MonoBehaviour
         if (ptr == IntPtr.Zero)
             return string.Empty;
         return Marshal.PtrToStringUTF8(ptr);
+    }
+    
+// a0 = -0.7595f, 0.21f (ei tarkka), -0.7595f
+    void GenerateGrid()
+    {
+        for (int z = 0; z < 8; z++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                var spawnedTile = Instantiate(Resources.Load("Prefabs/Ruutu"), new Vector3(-0.7595f + 0.217f * x, 0.3f, -0.7595f + 0.217f * z), Quaternion.identity);
+                spawnedTile.name = $"Tile {x}, {z}";
+            }
+        }
+    }
+
+    void GeneratePieces()
+    {
+        for (int x = 0; x < 8; x++)
+            Instantiate(Resources.Load("Prefabs/VS"), 
+                new Vector3(-0.7595f + 0.217f * x, 0.3f, -0.7595f + 0.217f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VT"), GameObject.Find("Tile 0,0").transform.position, Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VR"), GameObject.Find("Tile 0,0").transform.position, Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VL"), new Vector3(-0.7595f + 0.217f * 2, 0.3f, -0.7595f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VD"), new Vector3(-0.7595f + 0.217f * 3, 0.3f, -0.7595f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VK"), new Vector3(-0.7595f + 0.217f * 4, 0.3f, -0.7595f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VL"), new Vector3(-0.7595f + 0.217f * 5, 0.3f, -0.7595f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VR"), new Vector3(-0.7595f + 0.217f * 6, 0.3f, -0.7595f), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/VT"), new Vector3(-0.7595f + 0.217f * 7, 0.3f, -0.7595f), Quaternion.identity);
+        
+        for (int x = 0; x < 8; x++)
+            Instantiate(Resources.Load("Prefabs/MS"), 
+                new Vector3(-0.7595f + 0.217f * x, 0.3f, -0.7595f + 0.217f * 6), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MT"), new Vector3(-0.7595f, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MR"), new Vector3(-0.7595f + 0.217f, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/ML"), new Vector3(-0.7595f + 0.217f * 2, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MD"), new Vector3(-0.7595f + 0.217f * 3, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MK"), new Vector3(-0.7595f + 0.217f * 4, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/ML"), new Vector3(-0.7595f + 0.217f * 5, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MR"), new Vector3(-0.7595f + 0.217f * 6, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+        Instantiate(Resources.Load("Prefabs/MT"), new Vector3(-0.7595f + 0.217f * 7, 0.3f, -0.7595f + 0.217f * 7), Quaternion.identity);
+
+
+
+
+
+
+
     }
     
     //funktio siirron suorittamiselle (onko teleport siirto vai sulava siirto)
